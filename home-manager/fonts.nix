4acf13f122fe91706cc409ea816fcd832e342078
danila-clown <daphetad@gmail.com>
2025-10-02 01:06:12 +0000

    fonst

--- home-manager/fonts.nix
+++ home-manager/fonts.nix
@@ -1,14 +1,13 @@
-{ pkgs, ... }: {
-  home.fonts = {
-    enableDefaultPackages = true;
-    packages = with pkgs; [
-      noto-fonts
-      noto-fonts-cjk
-      noto-fonts-emoji
-      fira-code
-      jetbrains-mono
-      inter
-      liberation_ttf
-    ];
-  };
+{ pkgs, ... }: 
+
+{
+  home.packages = with pkgs; [
+    noto-fonts
+    noto-fonts-cjk-sans
+    noto-fonts-emoji
+    fira-code
+    jetbrains-mono
+    inter
+    liberation_ttf
+  ];
 }
